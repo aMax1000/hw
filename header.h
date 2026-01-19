2ce17ce0f00358689e8fbb1b1260b7b5d3ac2464
aMax1000 <mail2amax@gmail.com>
2026-01-19 16:07:34 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+int otterCoffeePrice(int a) {
+	switch (a)
+	{
+	case(1):
+		return 30;
+		break;
+	case(2):
+		return 50;
+		break;
+	default:
+		return 0;
+		break;
+	}
+}
