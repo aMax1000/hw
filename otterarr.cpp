7516853ad9c0bb3eaa79e466cdf591862458e99a
aMax1000 <mail2amax@gmail.com>
2026-03-11 15:13:23 +0200

    Update otterarr.cpp

--- otterarr.cpp
+++ otterarr.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-int allocationStepGlobal = 10;
 template <typename Tm1>
 void outarr(Tm1* arr, Tm1 size) {
     for (int i = 0; i < size; i++) {
@@ -10,12 +9,22 @@ void outarr(Tm1* arr, Tm1 size) {
 }
 
 template <typename T>
-void resize(T*& arr, int oldsize,int size) {
+void resize(T*& arr, int oldsize, int size) {
     T* arr3 = new T[size];
     for (int i = 0; i < oldsize; i++) {
         arr3[i] = arr[i];
     }
+    //outarr(arr, oldsize);
+    //outarr(arr3, size);
+    //arr3[0] = '1';
+    //outarr(arr, oldsize);
+    //outarr(arr3, size);
+    delete[] arr;
+    //outarr(arr3, size);
     arr = arr3;
+
+    //outarr(arr, oldsize);
+    //outarr(arr3, size);
 }
 
 
@@ -26,9 +35,9 @@ void addelat(T*& arr, int size, T ell, int place) {
     T temp;
     T temp2 = ell;
     for (int i = place; i < size; i++) {
-            temp = arr[i];
-            arr[i] = temp2;
-            temp2 = temp;
+        temp = arr[i];
+        arr[i] = temp2;
+        temp2 = temp;
     }
 }
 
@@ -43,39 +52,54 @@ void advresize(T*& arr, int oldsize, int size, int shift = 0, int cut = 0) {
             arr3[i] = arr[i];
         }
     }
+    delete[] arr;
     arr = arr3;
 }
 
 template <typename T>
 class arr2 {
 public:
-    T* arr = new T[0];
+    T* arr = nullptr;
     int size = 0;
     int allsize = 0;
     int allocationStep = 10;
     T defaultell = 0;
-    arr2() {};
+    arr2(arr2<T>& a) {
+        arr = new T[a.size];
+        size = a.size;
+        for (int i = 0; i < size; i++) {
+            arr[i] = a.arr[i];
+        }
+    }
+
     arr2(T*& arr2, int size2) {
-        push_back(arr2, size2);
+        arr=new T[size2];
+        size = size2;
+        for (int i = 0; i < size; i++) {
+            arr[i] = arr2[i];
+        }
+    }
+    ~arr2() {
+        delete[] arr;
     }
 
 
 
     //array push
 
-    void push_back(arr2<int> arr2) {
+    void push_back(arr2<T>& arr2) {
         push_back(arr2.arr, arr2.size);
     }
-    void push_front(arr2<int> arr2) {
+    void push_front(arr2<T>& arr2) {
         push_front(arr2.arr, arr2.size);
     }
-    void push_ell(arr2<int> arr2,int place) {
-        push_ell(arr2.arr, arr2.size,place);
+    void push_ell(arr2<T>& arr2, int place) {
+        push_ell(arr2.arr, arr2.size, place);
     }
 
-    void push_back(T*& arr2, int size2) {
+    void push_back(T* arr2, int size2) {
         if (size + size2 > allsize) {
-            allsize = size + size2 + allocationStep - (size + size2) % allocationStep;
+            allsize = size + size2 + allocationStep*(size2%allocationStep+1) - (size + size2) % allocationStep;
             resize(arr, size, allsize);
         }
         for (int i = 0; i < size2; i++) {
@@ -85,7 +109,7 @@ public:
     }
     void push_front(T*& arr2, int size2) {
         if (size + size2 > allsize) {
-            allsize = size + size2 + allocationStep - (size + size2) % allocationStep;
+            allsize = size + size2 + allocationStep * (size2 % allocationStep + 1) - (size + size2) % allocationStep;
         }
         advresize(arr, size, allsize, size2);
         for (int i = 0; i < size2; i++) {
@@ -93,21 +117,21 @@ public:
         }
         size += size2;
     }
-    void push_ell(T*& arr2, int size2,int place) {
+    void push_ell(T*& arr2, int size2, int place) {
         if (size + size2 > allsize) {
-            allsize = size + size2 + allocationStep - (size + size2) % allocationStep;
+            allsize = size + size2 + allocationStep * (size2 % allocationStep + 1) - (size + size2) % allocationStep;
         }
         advresize(arr, size, allsize, size2, place);
         for (int i = 0; i < size2; i++) {
-            arr[i+place] = arr2[i];
+            arr[i + place] = arr2[i];
         }
         size += size2;
     }
-    void pop_ell(int place,int len) {
-        if (size -len <= allsize-allocationStep) {
-            allsize = size - len +allocationStep - (size - len) % allocationStep;
+    void pop_ell(int place, int len) {
+        if (size - len <= allsize - allocationStep) {
+            allsize = size - len + allocationStep - (size - len) % allocationStep;
         }
-        advresize(arr, size, allsize, -len, place+len);
+        advresize(arr, size, allsize, -len, place + len);
         size -= len;
     }
     void pop_back(int len) {
@@ -139,7 +163,7 @@ public:
             allsize += allocationStep;
             resize(arr, size, allsize);
         }
-        arr[size-1] = ell;
+        arr[size - 1] = ell;
     }
 
 
@@ -155,25 +179,26 @@ public:
     }
 
 
-    void push_ell(T ell,int place) {
+    void push_ell(T ell, int place) {
         size++;
         if (size > allsize) {
             allsize += allocationStep;
             resize(arr, size, allsize);
         }
-        
-        if (place > size-1) {
+
+        if (place > size - 1) {
             if (place > allsize - 1) {
                 allsize = place + allocationStep - place % allocationStep;
                 resize(arr, size, allsize);
             }
-            
-            for (int i = size-1; i < place; i++) {
+
+            for (int i = size - 1; i < place; i++) {
                 arr[i] = defaultell;
             }
             size = place + 1;
             arr[place] = ell;
-        }else{
+        }
+        else {
             addelat(arr, size, ell, place);
         }
     }
@@ -229,10 +254,14 @@ public:
     }
 
     void print() {
+        std::cout << '{';
         for (int i = 0; i < size; i++) {
-            std::cout << arr[i] << ',';
+            if (i != 0) {
+                std::cout << ',';
+            }
+            std::cout << arr[i];
         }
-        std::cout << '\n';
+        std::cout << '}'<<'\n';
     }
 
     void srink() {
@@ -245,6 +274,14 @@ public:
         allsize = newsize;
     }
 
+    void reverce() {
+        T temp;
+        for (int i = 0; i < size / 2; i++) {
+            temp=arr[i];
+            arr[i] = arr[size - 1 - i];
+            arr[size - 1 - i] = temp;
+        }
+    }
 
 };
 
@@ -259,17 +296,21 @@ int main()
     }
     arr2<int> a(arr, size);
     arr2<int> b(arr, size);
-    arr2<int> c(arr, size);
+    arr2<int> c(arr,size);
+    arr2<int> d(a);
     for (int i = 0; i < c.size; i++) {
         c.arr[i] += 40;
     }
+    std::cout << a.arr << '\n';
+    std::cout << a.size << '\n';
+    outarr(a.arr, a.size);
     a.print();
     a.push_back(arr, size);
     a.print();
-    a.push_back(a);
+    a.push_back(b);
     a.print();
     cout << a.size << '\n';
-    cout << a.arr << '\n';//pointer
+    cout << a.arr << '\n';//pointer*/
     a.push_front(12);
     a.print();
     a.push_back(12);
@@ -286,23 +327,15 @@ int main()
     a.srink();
     a.pop_front();
     a.print();
-    a.push_ell(c,10);
+    a.push_ell(c, 10);
     a.print();
     a.pop_ell(10, 5);
     a.print();
     a.pop_front(5);
     a.print();
+    a.reverce();
+    a.print();
     a.pop_back(5);
     a.print();
+    d.print();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
